fa719db1f4fb5d0f4dd40b22180cc3b4977bc9ae
xhitori <122475441+xhitori@users.noreply.github.com>
2023-01-27 11:20:47 +0700

    Add files via upload

---
+++ jadianime.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw 'Reply Gambar nya'
+let media = await q.download()
+let url = await uploadImage(media)
+m.reply('Wait sabar lagi loading...')
+await conn.sendFile(m.chat, await (await fetch(`https://luciaximena.site/api/other/toanime?url=${url}&apikey=miracle`)).buffer(), 'anime.jpg', 'Ciee Jadi Anime :v', m)
+}
+handler.help = ['jadianime']
+handler.tags = ['convert']
+handler.command = /^(to|jadi)anime$/i
+handler.limit = 5
+
+export default handler
