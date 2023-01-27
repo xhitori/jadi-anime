40602065db0e8af7ac8710a686ce4dfedf43ee2a
xhitori <122475441+xhitori@users.noreply.github.com>
2023-01-27 11:23:21 +0700

    Lol

--- jadianime.js
+++ jadianime.js
@@ -8,7 +8,7 @@ if (!mime) throw 'Reply Gambar nya'
 let media = await q.download()
 let url = await uploadImage(media)
 m.reply('Wait sabar lagi loading...')
-await conn.sendFile(m.chat, await (await fetch(`https://luciaximena.site/api/other/toanime?url=${url}&apikey=miracle`)).buffer(), 'anime.jpg', 'Ciee Jadi Anime :v', m)
+await conn.sendFile(m.chat, await (await fetch(`https://luciaximena.site/api/other/toanime?url=${url}&apikey=YOURAPIKEY`)).buffer(), 'anime.jpg', 'Ciee Jadi Anime :v', m)
 }
 handler.help = ['jadianime']
 handler.tags = ['convert']
